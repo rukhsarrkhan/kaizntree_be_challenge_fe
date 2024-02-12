d0e2894f482fa8371c26eb14cc667ef92b301b6b
rukhsarrkhan <rukhsarkhan4198@gmail.com>
2024-02-11 22:32:02 -0500

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router basename="/kaizntree_be_challenge_fe">
+  <Router basename="/">
     <App />
   </Router>
 );
