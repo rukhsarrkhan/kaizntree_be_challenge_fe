3ff7466cbfc49177d11b4aef674d9f4ee0238a8f
rukhsarrkhan <rukhsarkhan4198@gmail.com>
2024-02-12 13:43:23 -0500

    Added validations to login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { loginFields } from "../constants/formField";
 import FormAction from "./FormAction";
 import FormExtra from "./FormExtra";
 import Input from "./Input";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 const fields = loginFields;
 let fieldsState = {};
-fields.forEach(field => fieldsState[field.id] = '');
+fields.forEach((field) => (fieldsState[field.id] = ""));
 let apiKey = "bjkabfk";
+
+let validationErrorsState = {};
+fields.forEach((field) => (validationErrorsState[field.id + "Error"] = ""));
 
 export default function Login() {
     const navigate = useNavigate();
@@ -16,41 +20,70 @@ export default function Login() {
     const [loginState, setLoginState] = useState(fieldsState);
     const [loginError, setLoginError] = useState('');
 
+    const [errors, setErrors] = useState(validationErrorsState);
+
+    const validateFields = () => {
+        let isValid = true;
+        let errors = {};
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(loginState.email)) {
+            errors.emailError = "Invalid email format.";
+            isValid = false;
+        }
+
+        if (loginState.password.length < 8) {
+            errors.passwordError = "Password must be at least 8 characters.";
+            isValid = false;
+        }
+
+        const passwordComplexityRegex = /^(?=.*[a-zA-Z])(?=.*\d)/;
+        if (!passwordComplexityRegex.test(loginState.password)) {
+            errors.passwordError = "Password must contain both letters and numbers.";
+            isValid = false;
+        }
+
+        setErrors(errors);
+        return isValid;
+    };
+
     const handleChange = (e) => {
         setLoginState({ ...loginState, [e.target.id]: e.target.value });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        authenticateUser();
+        if (validateFields()) {
+            authenticateUser();
+        }
     };
 
     //Handle Login API Integration here
     const authenticateUser = () => {
         const endpoint = `http://127.0.0.1:8000/api/v1/auth/login/`;
-        fetch(endpoint,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(loginState)
-            }).then(response => {
+        fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginState),
+        })
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 return response.json();
             })
-            .then(data => {
+            .then((data) => {
                 sessionStorage.setItem('access_token', data.access_token);
                 sessionStorage.setItem('refresh_token', data.refresh_token);
 
                 // const myValue = sessionStorage.getItem('myKey');
                 //sessionStorage.removeItem('myKey');
                 //sessionStorage.clear();
-                navigate('/dashboard');
+                navigate("/dashboard");
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log('Login failed:', error.message);
                 // Optionally, inform the user about the login failure and suggest next steps
                 // For example, updating the state to show an error message on the UI
@@ -63,8 +96,8 @@ export default function Login() {
     return (
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div className="-space-y-px">
-                {
-                    fields.map(field =>
+                {fields.map((field) => (
+                    <React.Fragment key={field.id}>
                         <Input
                             key={field.id}
                             handleChange={handleChange}
@@ -77,9 +110,9 @@ export default function Login() {
                             isRequired={field.isRequired}
                             placeholder={field.placeholder}
                         />
-
-                    )
-                }
+                        {errors[field.id + "Error"] && <p className="text-red-500">{errors[field.id + "Error"]}</p>}
+                    </React.Fragment>
+                ))}
             </div>
             {loginError && <div className="text-red-500 text-sm mt-2">{loginError}</div>}
             <FormExtra />
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,48 +1,85 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { signupFields } from "../constants/formField";
 import FormAction from "./FormAction";
 import Input from "./Input";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 const fields = signupFields;
 let fieldsState = {};
 
-fields.forEach(field => fieldsState[field.id] = '');
+fields.forEach((field) => (fieldsState[field.id] = ""));
+
+let validationErrorsState = {};
+fields.forEach((field) => (validationErrorsState[field.id + "Error"] = ""));
 
 export default function Signup() {
     const navigate = useNavigate();
 
     const [signupState, setSignupState] = useState(fieldsState);
     const [loginError, setLoginError] = useState('');
+    const [errors, setErrors] = useState(validationErrorsState);
+
+    const validateFields = () => {
+        let isValid = true;
+        let errors = {};
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(signupState.email)) {
+            errors.emailError = "Invalid email format.";
+            isValid = false;
+        }
+
+        if (signupState.password.length < 8) {
+            errors.passwordError = "Password must be at least 8 characters.";
+            isValid = false;
+        }
+
+        const passwordComplexityRegex = /^(?=.*[a-zA-Z])(?=.*\d)/;
+        if (!passwordComplexityRegex.test(signupState.password)) {
+            errors.passwordError = "Password must contain both letters and numbers.";
+            isValid = false;
+        }
+
+        if (signupState.password !== signupState.password2) {
+            errors.password2Error = "Passwords do not match.";
+            isValid = false;
+        }
+
+        setErrors(errors);
+        return isValid;
+    };
 
     const handleChange = (e) => setSignupState({ ...signupState, [e.target.id]: e.target.value });
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(signupState);
-        createAccount();
+        if (validateFields()) {
+            console.log(signupState);
+            createAccount();
+        }
     };
 
     //handle Signup API Integration here
     const createAccount = () => {
         const endpoint = `http://127.0.0.1:8000/api/v1/auth/register/`;
-        fetch(endpoint,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(signupState)
-            }).then(response => {
+        fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(signupState),
+        })
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 return response.json();
             })
-            .then(data => {
-                navigate('/login');
+            .then((data) => {
+                navigate("/login");
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log('Signup failed:', error.message);
                 // Optionally, inform the user about the login failure and suggest next steps
                 // For example, updating the state to show an error message on the UI
@@ -55,8 +92,8 @@ export default function Signup() {
     return (
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div className="">
-                {
-                    fields.map(field =>
+                {fields.map((field) => (
+                    <React.Fragment key={field.id}>
                         <Input
                             key={field.id}
                             handleChange={handleChange}
@@ -69,9 +106,9 @@ export default function Signup() {
                             isRequired={field.isRequired}
                             placeholder={field.placeholder}
                         />
-
-                    )
-                }
+                        {errors[field.id + "Error"] && <p className="text-red-500">{errors[field.id + "Error"]}</p>}
+                    </React.Fragment>
+                ))}
             </div>
             {loginError && <div className="text-red-500 text-sm mt-2">{loginError}</div>}
             <FormAction handleSubmit={handleSubmit} text="Signup" />
