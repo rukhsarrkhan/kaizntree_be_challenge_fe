3b6262be765aea85a8ad541bc0ff24c67b5de643
rukhsarrkhan <rukhsarkhan4198@gmail.com>
2024-02-11 22:22:56 -0500

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
+const basename = process.env.PUBLIC_URL;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename={basename}>
     <App />
   </Router>
 );
