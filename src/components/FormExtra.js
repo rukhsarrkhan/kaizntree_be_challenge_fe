2930f39c3ed1d87841c77a1bd4b6a76658cb814e
rukhsarrkhan <rukhsarkhan4198@gmail.com>
2024-02-11 19:13:56 -0500

    setup gh-pages

---
+++ src/components/FormExtra.js
@@ -0,0 +1,13 @@
+export default function FormExtra() {
+    return (
+        <div className="flex items-center justify-between ">
+
+            <div className="text-sm">
+                <a href="#" className="font-medium text-purple-600 hover:text-purple-500">
+                    Forgot your password?
+                </a>
+            </div>
+        </div>
+
+    );
+}
